3131009863afdb60e41d8fcca4e5788552504408
Gopinath1098 <justicegopi2917@gmail.com>
2026-07-26 16:23:24 +0530

    workflow secret changes

--- app/src/test/java/com/cloud/oms/app/AppApplicationTests.java
+++ app/src/test/java/com/cloud/oms/app/AppApplicationTests.java
@@ -9,8 +9,4 @@ class AppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	//2b595f90-dcaf-405f-8394-f18d79bbf996 - tenant id
-	//ec1ccc60-ab2b-4e07-a8ce-0cb237eddbd9 - client id
-	// FvA8Q~L.xBhB0y1REZtHRFnNddjE4C_0sz4szcBN - client secret
-
 }
